9e2128b653fd74a05aaf76e642c2830dc5c91582
Fabio Muller <fabioml10@yahoo.com.br>
2021-05-12 11:36:43 -0300

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp ({ Component, pageProps }) {
+  return (
+    <>
+      {/* Head pode existir em quanquer página */}
+      <Head>
+        <title>OneBitFood</title>
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
 
 export default MyApp
