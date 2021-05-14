529beb1b1d8adc5c4d5d58d6bd87456570697999
Fabio Muller <fabioml10@yahoo.com.br>
2021-05-14 16:56:37 -0300

    Use SSR and SSG

--- components/DetailsRestaurant/index.js
+++ components/DetailsRestaurant/index.js
@@ -1,27 +1,20 @@
 import React from 'react'
 import Details from './Details'
 import CategoryProducts from './CategoryProducts'
-import getRestaurant from '../../services/getRestaurant'
-import { useRouter } from 'next/router'
-import { Spinner, Alert } from 'react-bootstrap'
+// SSR
+// import { Alert } from 'react-bootstrap'
 
 export default function DetailsRestaurant (props) {
-  const router = useRouter()
-  const { id } = router.query
-
-  const { restaurant, isLoading, isError } = getRestaurant(id)
-
-  if (isError) {
-    return <Alert variant='custom-red'>Erro ao carregar</Alert>
-  } else if (isLoading) {
-    return <Spinner animation='border' />
-  }
+  // SSR
+  // if (props.isError) {
+  //   return <Alert variant='custom-red'>Erro ao carregar</Alert>
+  // }
 
   return (
     <>
-      <Details {...restaurant} />
-      {restaurant.product_categories.map((productCategory, i) =>
-        <CategoryProducts restaurant={restaurant} {...productCategory} key={i} />
+      <Details {...props.restaurant} />
+      {props.restaurant.product_categories.map((productCategory, i) =>
+        <CategoryProducts restaurant={props.restaurant} {...productCategory} key={i} />
       )}
     </>
   )
